e4d5b817f19bc8af77e2f74024bf493cc3763dbd
Philip <12156105+Scobalula@users.noreply.github.com>
2025-07-06 11:19:55 +0100

    LZ4 Codec, VFS, and Span Reader

---
+++ src/cs/RedFox.IO/FileSystem/VirtualDirectory.cs
@@ -0,0 +1,344 @@
+﻿// --------------------------------------------------------------------------------------
+// RedFox Utility Library
+// --------------------------------------------------------------------------------------
+// Copyright (c) 2025 Philip/Scobalula
+// --------------------------------------------------------------------------------------
+// Please see LICENSE.md for license information.
+// This library is also bound by 3rd party licenses.
+// --------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedFox.IO.FileSystem
+{
+    /// <summary>
+    /// A class to hold and manipulate a virtual directory that contains files and sub-directories.
+    /// </summary>
+    public class VirtualDirectory
+    {
+        /// <summary>
+        /// Directories stored within this directory.
+        /// </summary>
+        internal readonly HashSet<VirtualDirectory> _directories =[];
+
+        /// <summary>
+        /// Files stored within this directory.
+        /// </summary>
+        internal readonly HashSet<VirtualFile> _files = [];
+
+        /// <summary>
+        /// Gets the parent directory.
+        /// </summary>
+        public VirtualDirectory? Parent { get; private set; }
+
+        /// <summary>
+        /// Gets or Sets the name of the directory.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the full path of the directory.
+        /// </summary>
+        public string FullPath => Parent == null ? Name : Path.Combine(Parent.FullPath, Name);
+
+        /// <summary>
+        /// Gets the directories stored within this directory.
+        /// </summary>
+        public IReadOnlyCollection<VirtualDirectory> Directories => _directories;
+
+        /// <summary>
+        /// Gets the files stored within this directory.
+        /// </summary>
+        public IReadOnlyCollection<VirtualFile> Files => _files;
+
+        /// <remarks>
+        /// Initializes a new instance of the <see cref="VirtualDirectory"/>.
+        /// </remarks>
+        internal VirtualDirectory()
+        {
+            Name = string.Empty;
+        }
+
+        /// <remarks>
+        /// Initializes a new instance of the <see cref="VirtualDirectory"/> with the provided name.
+        /// </remarks>
+        /// <param name="name">The name of the directory.</param>
+        public VirtualDirectory(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name), "Empty name provided.");
+            if (name.IndexOfAny(VirtualFileSystem.InvalidFileNameChars) > -1)
+                throw new ArgumentException("Name contains invalid characters.", nameof(name));
+
+            Name = name;
+        }
+
+        /// <summary>
+        /// Moves the this directory into the provided directory.
+        /// </summary>
+        /// <param name="newParent">The directory to move this directory into.</param>
+        public void MoveTo(VirtualDirectory? newParent)
+        {
+            if (newParent == Parent)
+                return;
+
+            Parent?._directories.Remove(this);
+            Parent = newParent;
+
+            if (newParent is not null && !newParent._directories.Add(this))
+            {
+                throw new IOException($"Directory: {newParent.FullPath} already contains a directory with the name: {Name}");
+            }
+        }
+
+        /// <summary>
+        /// Create a new directory to this directory.
+        /// </summary>
+        /// <param name="name">The name of the directory.</param>
+        /// <returns>The resulting directory.</returns>
+        public VirtualDirectory CreateDirectory(string name)
+        {
+            var newDirectory = new VirtualDirectory(name);
+            newDirectory.MoveTo(this);
+            return newDirectory;
+        }
+
+        /// <summary>
+        /// Creates a new directory and directories within this directory.
+        /// </summary>
+        /// <param name="fullPath">The full directory path.</param>
+        /// <returns>The resulting directory at the end of the path.</returns>
+        public VirtualDirectory? CreateDirectories(string? fullPath)
+        {
+            if (string.IsNullOrWhiteSpace(fullPath))
+                return null;
+
+            var parts = fullPath.Split(['\\', '/'], StringSplitOptions.RemoveEmptyEntries);
+
+            var current = this;
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                current = current._directories.FirstOrDefault(x => x.Name.Equals(parts[i], StringComparison.OrdinalIgnoreCase)) ?? current.CreateDirectory(parts[i]);
+            }
+
+            return current;
+        }
+
+        public bool TryGetDirectory(string fullPath, [NotNullWhen(true)] out VirtualDirectory? directory)
+        {
+            directory = null;
+
+            if (string.IsNullOrWhiteSpace(fullPath))
+                return false;
+
+            string[] parts = fullPath.Split(['\\', '/'], StringSplitOptions.RemoveEmptyEntries);
+
+            var current = this;
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                string dirName = parts[i];
+
+                var dir = current._directories.FirstOrDefault(x => x.Name.Equals(dirName, StringComparison.OrdinalIgnoreCase));
+
+                if (dir is null)
+                {
+                    return false;
+                }
+
+                current = dir;
+            }
+
+            directory = current;
+            return true;
+        }
+
+        /// <summary>
+        /// Returns an enumerable collection of the directories that meet specified criteria.
+        /// </summary>
+        /// <returns>An enumerable collection of the directories within the directory that match the specified search pattern and search option.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the directory could not be found.</exception>
+        public IEnumerable<VirtualDirectory> EnumerateDirectories() => EnumerateDirectories(null, "*", SearchOption.TopDirectoryOnly);
+
+        /// <summary>
+        /// Returns an enumerable collection of the directories that meet specified criteria.
+        /// </summary>
+        /// <param name="path">The path to the directory to search. This string is not case-sensitive. If null or empty, the search starts from this directory.</param>
+        /// <returns>An enumerable collection of the directories within the directory that match the specified search pattern and search option.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the directory could not be found.</exception>
+        public IEnumerable<VirtualDirectory> EnumerateDirectories(string? path) => EnumerateDirectories(path, "*", SearchOption.TopDirectoryOnly);
+
+        /// <summary>
+        /// Returns an enumerable collection of the directories that meet specified criteria.
+        /// </summary>
+        /// <param name="path">The path to the directory to search. This string is not case-sensitive. If null or empty, the search starts from this directory.</param>
+        /// <param name="searchPattern">The search string to match against the names.</param>
+        /// <returns>An enumerable collection of the directories within the directory that match the specified search pattern and search option.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the directory could not be found.</exception>
+        public IEnumerable<VirtualDirectory> EnumerateDirectories(string? path, string searchPattern) => EnumerateDirectories(path, searchPattern, SearchOption.TopDirectoryOnly);
+
+        /// <summary>
+        /// Returns an enumerable collection of the directories that meet specified criteria.
+        /// </summary>
+        /// <param name="path">The path to the directory to search. This string is not case-sensitive. If null or empty, the search starts from this directory.</param>
+        /// <param name="searchPattern">The search string to match against the names.</param>
+        /// <param name="searchOption">Specified the search option indicating whether to search all directories or only the top directory.</param>
+        /// <returns>An enumerable collection of the directories within the directory that match the specified search pattern and search option.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the directory could not be found.</exception>
+        public IEnumerable<VirtualDirectory> EnumerateDirectories(string? path, string searchPattern, SearchOption searchOption)
+        {
+            var directory = this;
+
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                if (!TryGetDirectory(path, out directory))
+                {
+                    throw new DirectoryNotFoundException($"Failed to find directory: {path}");
+                }
+            }
+
+            foreach (var dir in directory._directories)
+            {
+                if (FileSystemName.MatchesWin32Expression(searchPattern, dir.Name))
+                    yield return dir;
+
+                if (searchOption == SearchOption.AllDirectories)
+                {
+                    foreach (var subDir in dir.EnumerateDirectories(null, searchPattern, searchOption))
+                    {
+                        if (FileSystemName.MatchesWin32Expression(searchPattern, subDir.Name))
+                            yield return subDir;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerable collection of the files that meet specified criteria.
+        /// </summary>
+        /// <returns>An enumerable collection of the files within the directory that match the specified search pattern and search option.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the directory could not be found.</exception>
+        public IEnumerable<VirtualFile> EnumerateFiles() => EnumerateFiles(null, "*", SearchOption.TopDirectoryOnly);
+
+        /// <summary>
+        /// Returns an enumerable collection of the files that meet specified criteria.
+        /// </summary>
+        /// <param name="path">The path to the directory to search. This string is not case-sensitive. If null or empty, the search starts from this directory.</param>
+        /// <returns>An enumerable collection of the files within the directory that match the specified search pattern and search option.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the directory could not be found.</exception>
+        public IEnumerable<VirtualFile> EnumerateFiles(string? path) => EnumerateFiles(path, "*", SearchOption.TopDirectoryOnly);
+
+        /// <summary>
+        /// Returns an enumerable collection of the files that meet specified criteria.
+        /// </summary>
+        /// <param name="path">The path to the directory to search. This string is not case-sensitive. If null or empty, the search starts from this directory.</param>
+        /// <param name="searchPattern">The search string to match against the names.</param>
+        /// <returns>An enumerable collection of the files within the directory that match the specified search pattern and search option.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the directory could not be found.</exception>
+        public IEnumerable<VirtualFile> EnumerateFiles(string? path, string searchPattern) => EnumerateFiles(path, searchPattern, SearchOption.TopDirectoryOnly);
+
+        /// <summary>
+        /// Returns an enumerable collection of the files that meet specified criteria.
+        /// </summary>
+        /// <param name="path">The path to the directory to search. This string is not case-sensitive. If null or empty, the search starts from this directory.</param>
+        /// <param name="searchPattern">The search string to match against the names.</param>
+        /// <param name="searchOption">Specified the search option indicating whether to search all directories or only the top directory.</param>
+        /// <returns>An enumerable collection of the files within the directory that match the specified search pattern and search option.</returns>
+        /// <exception cref="DirectoryNotFoundException">Thrown if the directory could not be found.</exception>
+        public IEnumerable<VirtualFile> EnumerateFiles(string? path, string searchPattern, SearchOption searchOption)
+        {
+            var directory = this;
+
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                if (!TryGetDirectory(path, out directory))
+                {
+                    throw new DirectoryNotFoundException($"Failed to find directory: {path}");
+                }
+            }
+
+            foreach (var file in directory._files)
+            {
+                if (FileSystemName.MatchesWin32Expression(searchPattern, file.Name))
+                    yield return file;
+            }
+
+            if (searchOption == SearchOption.AllDirectories)
+            {
+                foreach (var dir in EnumerateDirectories(null, "*", searchOption))
+                {
+                    foreach (var file in dir._files)
+                    {
+                        if (FileSystemName.MatchesWin32Expression(searchPattern, file.Name))
+                            yield return file;
+                    }
+                }
+            }
+        }
+
+        public VirtualFile GetFile(string path)
+        {
+            if (!TryGetFile(path, out var file))
+                throw new FileNotFoundException("Could not find file.", path);
+
+            return file;
+        }
+
+        public bool TryGetFile(string path, [NotNullWhen(true)] out VirtualFile? file)
+        {
+            file = null;
+
+            if (string.IsNullOrWhiteSpace(path))
+                return false;
+
+            string[] parts = path.Split(['\\', '/'], StringSplitOptions.RemoveEmptyEntries);
+
+            var current = this;
+
+            for (int i = 0; i < parts.Length - 1; i++)
+            {
+                string dirName = parts[i];
+
+                var dir = current._directories.FirstOrDefault(x => x.Name.Equals(dirName, StringComparison.OrdinalIgnoreCase));
+
+                if (dir is null)
+                {
+                    return false;
+                }
+
+                current = dir;
+            }
+
+            file = current._files.FirstOrDefault(x => x.Name == parts[^1]);
+            return file is not null;
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return Name.ToLower().GetHashCode();
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object? obj)
+        {
+            if (obj is VirtualDirectory directory)
+                return directory.Name.Equals(Name, StringComparison.OrdinalIgnoreCase);
+
+            return base.Equals(obj);
+        }
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ src/cs/RedFox.IO/SpanReader.cs
@@ -0,0 +1,126 @@
+﻿// --------------------------------------------------------------------------------------
+// RedFox Utility Library
+// --------------------------------------------------------------------------------------
+// Copyright (c) 2025 Philip/Scobalula
+// --------------------------------------------------------------------------------------
+// Please see LICENSE.md for license information.
+// This library is also bound by 3rd party licenses.
+// --------------------------------------------------------------------------------------
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace RedFox.IO
+{
+    /// <summary>
+    /// Provides a simple method to read continous data from a <see cref="Span{T}"/> byte buffer.
+    /// </summary>
+    /// <param name="buffer">The buffer to read from.</param>
+    public ref struct SpanReader(ReadOnlySpan<byte> buffer)
+    {
+        private readonly ReadOnlySpan<byte> _buffer = buffer;
+
+        private int _position = 0;
+
+        /// <summary>
+        /// Gets or Sets the current position within the buffer.
+        /// </summary>
+        public int Position { readonly get { return _position; } set { Seek(value, SeekOrigin.Begin); } }
+
+        /// <summary>
+        /// Reads a read-only byte buffer from the buffer of the given size and advances the position by the size of the data.
+        /// </summary>
+        /// <param name="size">The size of the byte buffer to read.</param>
+        /// <returns>Resulting read-only byte buffer.</returns>
+        public ReadOnlySpan<byte> Read(int size)
+        {
+            var result = _buffer.Slice(_position, size);
+            _position += size;
+            return result;
+        }
+
+        /// <summary>
+        /// Reads a read-only byte buffer from the buffer of the given size. This does not modify the current position.
+        /// </summary>
+        /// <param name="position">The size of the byte buffer to read.</param>
+        /// <param name="size">The size of the byte buffer to read.</param>
+        /// <returns>Resulting read-only byte buffer.</returns>
+        public readonly ReadOnlySpan<byte> Read(int position, int size) => _buffer.Slice(position, size);
+
+        /// <summary>
+        /// Reads the value of the given type from the buffer and advances the position by the size of the element.
+        /// </summary>
+        /// <typeparam name="T">The type to read from the buffer, this must be an unmanaged plain data type.</typeparam>
+        /// <returns>Resulting type from the buffer.</returns>
+        public T Read<T>() where T : unmanaged
+        {
+            var result = MemoryMarshal.Read<T>(_buffer[_position..]);
+            _position += Unsafe.SizeOf<T>();
+            return result;
+        }
+
+        /// <summary>
+        /// Reads the value of the given type from the buffer at the given position. This does not modify the current position.
+        /// </summary>
+        /// <typeparam name="T">The type to read from the buffer, this must be an unmanaged plain data type.</typeparam>
+        /// <param name="position">The absolute position of the elements within the buffer.</param>
+        /// <param name="count">The number of elements to read.</param>
+        /// <returns>Resulting type from the buffer.</returns>
+        public readonly ReadOnlySpan<T> Read<T>(int position, int count) where T : unmanaged => MemoryMarshal.Cast<byte, T>(Read(position, Unsafe.SizeOf<T>() * count));
+
+        /// <summary>
+        /// Reads a UTF-8 encoded string from the buffer of the size and advances the position by the size of the data.
+        /// </summary>
+        /// <param name="size">The size of the string to read.</param>
+        /// <returns>Resulting string.</returns>
+        public string ReadString(int size)
+        {
+            var result = Encoding.UTF8.GetString(_buffer.Slice(_position, size));
+            _position += size;
+            return result;
+        }
+
+        /// <summary>
+        /// Reads a UTF-8 encoded string from the buffer of the size at the given position. This does not modify the current position.
+        /// </summary>
+        /// <param name="position">The size of the string to read.</param>
+        /// <param name="size">The size of the string to read.</param>
+        /// <returns>Resulting string.</returns>
+        public readonly string ReadString(int position, int size) => Encoding.UTF8.GetString(_buffer.Slice(position, size));
+
+        /// <summary>
+        /// Seeks to the provided offset within the buffer for the given seek origin.
+        /// </summary>
+        /// <param name="offset">The offset to seek to.</param>
+        /// <param name="origin">The origin to seek from.</param>
+        /// <returns>New buffer position.</returns>
+        /// <exception cref="NotImplementedException">Thrown if an attempt to seek to an umimplemented seek origin is performed.</exception>
+        /// <exception cref="EndOfStreamException">Thrown if an attempt to seek past the end of the buffer is performed.</exception>
+        /// <exception cref="IOException">Thrown if an attempt to seek to before the start of the buffer is performed.</exception>
+        public long Seek(int offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    _position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    _position += offset;
+                    break;
+                case SeekOrigin.End:
+                    _position = _buffer.Length - offset;
+                    break;
+                default:
+                    throw new NotImplementedException(origin.ToString());
+            }
+
+            if (_position >= _buffer.Length)
+                throw new EndOfStreamException("Attempted to seek past the end of the buffer.");
+            if (_position < 0)
+                throw new IOException("Attempted to seek before the start of the buffer.");
+
+            return _position;
+        }
+    }
+}
